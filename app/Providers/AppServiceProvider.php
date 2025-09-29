b09ec9e33d00cfcfa259c6ea14679e33ceb069a2
Rubia Cacemiro <rubiarobertac@gmail.com>
2025-09-28 22:51:14 -0300

    Feature | URL segura com HTTPS.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Inertia::share([
             'auth' => function () {
                 $user = auth()->user();
