03e994b00cd66f38d8b8b98532164a8da4fae42c
Rubia Cacemiro <rubiarobertac@gmail.com>
2025-10-30 20:03:24 -0300

    Feature | HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,15 +6,22 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Mail\MailManager;
 use App\Mail\SendGridTransport;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function register() {}
+    public function register()
+    {
+    }
 
     public function boot()
     {
         $this->app->make(MailManager::class)->extend('sendgrid', function ($config) {
             return new SendGridTransport(env('SENDGRID_API_KEY'), Log::channel('stack'));
         });
+
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
